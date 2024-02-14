0b0a099d97ca9dc30226a9184289a5abd8816ed2
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-02-14 23:19:16 +0530

    dyna roof backed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,53 +1,88 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default"
+    data-assets-path="../assets/" data-template="vertical-menu-template-free">
 
 <head>
-    <title>Login | Dyna Roof </title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <!-- jQuery library -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <title>Login DynaRoof</title>
 
-    <!-- Popper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <meta name="description" content="" />
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
 
-    <link rel="stylesheet" href="css/style.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="../assets/vendor/css/pages/page-auth.css" />
+    <!-- Helpers -->
+    <script src="../assets/vendor/js/helpers.js"></script>
+    <script src="../assets/js/config.js"></script>
 </head>
 
 <body>
-    <div class="login-form">
-        <div class="card">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card-image ">
-                        <img src='bg-3.jpg' alt='dynaroof logo'/>
-                    </div>
-                </div>
-                <div class="col-md-6">
+    <!-- Content -->
+
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register -->
+                <div class="card">
                     <div class="card-body">
-                        <h4>Welcome Back! Admin</h4>
-                        <p class="mb-0" style="color:#5d5d5d">Dynaroof Login -----</p>
-                        <form class="form-horizontal">
-                            <div class="form-group">
-                                <label for="" style="font-size:13px; font-weight:500; color:#494949">Email:</label>
-                                <input type="email" name="email" class="form-control" placeholder="Enter email ">
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="index.html" class="app-brand-link gap-2">
+                                <span class="app-brand-logo demo">
+                                    <img src="dynaroof-logo.png" alt="dynaroof logo" style="height:60px;" />
+                                </span>
+                            </a>
+
+                        </div>
+                        <!-- /Logo -->
+                        <h4 class="mb-2">Welcome Back! 👋</h4>
+                        <p class="mb-4">Please sign-in to your account</p>
+
+                        <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email Id</label>
+                                <input type="text" class="form-control" id="email" name="email-username"
+                                    placeholder="Enter your email or username" autofocus autocomplete="OFF"/>
                             </div>
-                            <div class="form-group">
-                                <label for="" style="font-size:13px; font-weight:500; color:#494949">Password:</label>
-                                <input type="password" name="password" class="form-control" placeholder="Enter password ">
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Password</label>
+                                    <a href="auth-forgot-password-basic.html">
+                                        <small>Forgot Password?</small>
+                                    </a>
+                                </div>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                                        aria-describedby="password" autocomplete="OFF"/>
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
                             </div>
-                            <div class="form-group">
-                                <p class="forgot-password-link mb-0"><a href="#" >Forgot password?</a></p>
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" name="submit" class="btn btn-success btn-md btn-login" value="Login">
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
                             </div>
                         </form>
                     </div>
@@ -55,6 +90,26 @@
             </div>
         </div>
     </div>
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 
 </html>
