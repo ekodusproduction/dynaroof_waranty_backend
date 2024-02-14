c886715d12b152a83745cacb6a641ded70b8a5ac
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-02-14 16:23:02 +0530

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Login | Dyna Roof </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <div class="login-form">
+        <div class="card">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card-image ">
+                        <img src='bg-3.jpg' alt='dynaroof logo'/>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card-body">
+                        <h4>Welcome Back! Admin</h4>
+                        <p class="mb-0" style="color:#5d5d5d">Dynaroof Login -----</p>
+                        <form class="form-horizontal">
+                            <div class="form-group">
+                                <label for="" style="font-size:13px; font-weight:500; color:#494949">Email:</label>
+                                <input type="email" name="email" class="form-control" placeholder="Enter email ">
+                            </div>
+                            <div class="form-group">
+                                <label for="" style="font-size:13px; font-weight:500; color:#494949">Password:</label>
+                                <input type="password" name="password" class="form-control" placeholder="Enter password ">
+                            </div>
+                            <div class="form-group">
+                                <p class="forgot-password-link mb-0"><a href="#" >Forgot password?</a></p>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="submit" class="btn btn-success btn-md btn-login" value="Login">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
