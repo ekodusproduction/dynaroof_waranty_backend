5427f8410971392c6a1030702a74e0bac867c621
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-03-07 14:10:38 +0530

    terms added trait added

--- app/Http/Controllers/Warranty/WarrantyCardController.php
+++ app/Http/Controllers/Warranty/WarrantyCardController.php
@@ -65,19 +65,22 @@ class WarrantyCardController extends Controller
             // Save the PDF to the public folder
             file_put_contents(public_path($pdfFileName), $pdfContents);
 
-            Warranty::create([
-                'customer_id' => $get_customers->id,
-                'card_link' => $pdfFileName,
-                'warranty_issue_date' => Carbon::now(),
-                'warranty_valid_till' => $warranty_valid_till
-            ]);
+            Session::flash('success', 'Great! Warranty card generated successfully.');
 
-            Customer::where('id', $get_customers->id)->update([
-                'is_warranty_issued' => 1
-            ]);
+            return response()->download(public_path($pdfFileName), $pdfFileName);
 
-            return back()->with('success', 'Great! Waranty card generated successfully.');
-            
+            // Warranty::create([
+            //     'customer_id' => $get_customers->id,
+            //     'card_link' => $pdfFileName,
+            //     'warranty_issue_date' => Carbon::now(),
+            //     'warranty_valid_till' => $warranty_valid_till
+            // ]);
+
+            // Customer::where('id', $get_customers->id)->update([
+            //     'is_warranty_issued' => 1
+            // ]);
+
+            // return back()->with('success', 'Great! Waranty card generated successfully.');
         }catch(\Exception $e){
             return $this->error('Oops! Something went wrong'.$e->getMessage().' Line number ==>'.$e->getLine(), null, null, 500);
         }
